f5427fa226dac0fe599cf1410f5f54a19be32094
Christopher Evans <cevans88@hotmail.co.uk>
2018-05-24 19:26:06 +0100

    Updated emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,25 +1,35 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'dracula t)
 (setq inhibit-startup-screen t)
+(require 'package) ;; You might already have this line
 (require 'package) ;; You might already have this line
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
-       (url (concat (if no-ssl "http" "http") "://melpa.org/packages/")))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
   (add-to-list 'package-archives (cons "melpa" url) t))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (monokai-theme zenburn-theme))))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
+ '(custom-safe-themes
+   (quote
+    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+ '(package-selected-packages
+   (quote
+    (haskell-mode color-theme-sanityinc-solarized zenburn-theme php-mode pandoc-mode monokai-theme markdown-mode evil color-theme-sanityinc-tomorrow))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(load-theme 'monokai t)
+(load "C:/Program Files (x86)/SICStus Prolog VC14 4.3.5/emacs/sicstus_emacs_init")
+(add-hook 'text-mode-hook 'turn-on-visual-line-mode)
